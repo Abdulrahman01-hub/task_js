eff32ff7b2da9ec3cede13838cdda6bfe96b0a6b
Abdulrahman01-hub <barbarossa.123456.1@gmail.com>
2025-09-15 17:45:20 +0400

    task

--- json.js
+++ json.js
@@ -1,9 +1,9 @@
 // task 1
-console.log("My name is Abdul");
+// console.log("My name is Abdul");
 
 // task2
-const name = "Abdul";
-console.log(name);
+// const name = "Abdul";
+// console.log(name);
 
 // task 3
 // Bu adlardan hansının dəyişən üçün istifadə oluna biləcəyini göstərin.
@@ -19,15 +19,15 @@ console.log(name);
 // Kitabın qiyməti
 
 // task4
-let bookPrice = 15;
-let bookAmount = 120;
-let totalValue = bookPrice * bookAmount;
-console.log("Depodaki bütün kitablarin ümumi dəyəri: " + totalValue + " AZN");
+// let bookPrice = 15;
+// let bookAmount = 120;
+// let totalValue = bookPrice * bookAmount;
+// console.log("Depodaki bütün kitablarin ümumi dəyəri: " + totalValue + " AZN");
 
 // task 5
-let kmhSpeed = 90;
-let msSpeed = kmhSpeed * 1000 / 3600;
-console.log("Sürət m/s: " + msSpeed);
+// let kmhSpeed = 90;
+// let msSpeed = kmhSpeed * 1000 / 3600;
+// console.log("Sürət m/s: " + msSpeed);
 
 // task 6
 // let dogumTarixi = prompt("Doğum tarixinizi daxil edin (YYYY-MM-DD formatinda):");
@@ -39,9 +39,9 @@ console.log("Sürət m/s: " + msSpeed);
 // console.log("Siz " + gunler + " gün yaşamisiniz!");
 
 // task 7
-let num = 12;
-console.log("Beşinci dərəcə qüvvət: " + (num ** 5));
-console.log("3-ə bölünmədən qaliği: " + ((num ** 5) % 3));
+// let num = 12;
+// console.log("Beşinci dərəcə qüvvət: " + (num ** 5));
+// console.log("3-ə bölünmədən qaliği: " + ((num ** 5) % 3));
 
 
 // task 8
@@ -68,20 +68,63 @@ console.log("3-ə bölünmədən qaliği: " + ((num ** 5) % 3));
 // console.log("Baş hərflər: " + initials);
 
 // task 12
-let fullName = prompt("Tam adinizi 'AD SOYAD' formatinda daxil edin:");
-let parts = fullName.split(" ");
-let reversed = parts[1] + " " + parts[0];
-console.log("SOYAD AD formatinda: " + reversed);
+// let fullName = prompt("Tam adinizi 'AD SOYAD' formatinda daxil edin:");
+// let parts = fullName.split(" ");
+// let reversed = parts[1] + " " + parts[0];
+// console.log("SOYAD AD formatinda: " + reversed);
 
 // task 13
-let ad = prompt("Adinizi daxil edin:");
-console.log(`Salam, ${ad}!`);
+// let ad = prompt("Adinizi daxil edin:");
+// console.log(`Salam, ${ad}!`);
 
 // task 14
-let num1 = Number(prompt("Birinci ədədi daxil edin:"));
-let num2 = Number(prompt("İkinci ədədi daxil edin:"));
+// let num1 = Number(prompt("Birinci ədədi daxil edin:"));
+// let num2 = Number(prompt("İkinci ədədi daxil edin:"));
 
-console.log("Toplama: " + (num1 + num2));
-console.log("Çixma: " + (num1 - num2));
-console.log("Vurma: " + (num1 * num2));
-console.log("Bölmə: " + (num1 / num2));
+// console.log("Toplama: " + (num1 + num2));
+// console.log("Çixma: " + (num1 - num2));
+// console.log("Vurma: " + (num1 * num2));
+// console.log("Bölmə: " + (num1 / num2));
+
+
+
+// task 15
+// let a = 12345;
+// let b = 98765;
+// let hasil = a * b;
+// let str = String(hasil);
+// console.log(hasil);
+// let text = str[5];
+// console.log("6-ci rəqəm:", text);
+
+// task 17
+// let num = prompt("4 rəqəmli ədəd daxil edin:");
+// let digits = num.split("");
+// let sum = Number(digits[0]) + Number(digits[1]) + Number(digits[2]) + Number(digits[3]);
+// console.log(sum);
+
+// task 19
+// let price = prompt("Qiyməti daxil edin:");
+// let parts = price.split(".");
+// let manat = parts[0];
+// let qepik = parts[1] || "00";
+// console.log(manat , "AZN" , qepik , "qepik");
+
+// task 20
+// let salary = prompt("Əmək haqqinizi daxil edin:");
+// salary = Number(salary);
+// let Salary2 = salary * (1 - 0.13);
+// console.log("Vergi ilə emek haqqiniz: " + Salary2 + " AZN");
+
+// task 21
+// let fullName = prompt("Soyad, ad və ata adinizi daxil edin:");
+// let parts = fullName.split(" ");
+// let result = parts[0] + " " + parts[1][0] + " " + parts[2][0] + ".";
+// console.log(result);
+
+// task 22
+// let price = Number(prompt("emanətin mebleğini daxil edin:"));
+// let years = Number(prompt("Müddeti il daxil edin:"));
+// let rate = Number(prompt("Faiz dərəcəsini (%) daxil edin:"));
+// let profit = price * rate / 100 * years; 
+// console.log("Mənfəət: " + profit + " AZN"); 
